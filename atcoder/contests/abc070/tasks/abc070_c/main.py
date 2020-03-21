ebd149438039831dd0476fdfcf00e0ba0ab7a5a4
aksh-t <tanno.ao@gmail.com>
2020-03-22 01:48:39 +0900

    素因数分解した結果をいい感じにマージすることで最小公倍数の因数を探し出す

--- atcoder/contests/abc070/tasks/abc070_c/main.py
+++ atcoder/contests/abc070/tasks/abc070_c/main.py
@@ -1,22 +1,31 @@
+from collections import defaultdict
+
 N = int(input())
 Ts = list(set([int(input()) for _ in range(N)]))
 
-def get_divider(n):
-    divider = set()
-    for i in range(1, n):
+def get_prime_factor(n):
+    res = defaultdict(int)
+    for i in range(2, n):
         if i * i > n:
             break
+        while n % i == 0:
+            res[i] = res[i] + 1
+            n = n / i
+    if n != 1:
+        res[n] = 1
+    return res
 
-        if n % i == 0:
-            divider.add(i)
-            if i != int(n / i):
-                divider.add(int(n / i))
-    return divider
+def merge_prime_factor(a, b):
+    for k, v in b.items():
+        a[k] = v if a[k] < v else a[k]
+    return a
 
 
-divider_set = set()
+lcm_prime_factor = defaultdict(int)
 for T in Ts:
-    divider_set = divider_set.union(get_divider(T))
-divider_set.discard(1)
-print(str(min(divider_set)))
+    lcm_prime_factor = merge_prime_factor(lcm_prime_factor, get_prime_factor(T))
 
+res = 1
+for k, v in lcm_prime_factor.items():
+    res = res * (k ** v)
+print(res)
