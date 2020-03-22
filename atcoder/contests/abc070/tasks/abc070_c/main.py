e4af1116b54b4e48d712fb00e05a2d0452539893
aksh-t <tanno.ao@gmail.com>
2020-03-22 23:27:55 +0900

    解説をみて変更したが、ACせず

--- atcoder/contests/abc070/tasks/abc070_c/main.py
+++ atcoder/contests/abc070/tasks/abc070_c/main.py
@@ -3,29 +3,16 @@ from collections import defaultdict
 N = int(input())
 Ts = list(set([int(input()) for _ in range(N)]))
 
-def get_prime_factor(n):
-    res = defaultdict(int)
-    for i in range(2, n):
-        if i * i > n:
-            break
-        while n % i == 0:
-            res[i] = res[i] + 1
-            n = n / i
-    if n != 1:
-        res[n] = 1
-    return res
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
 
-def merge_prime_factor(a, b):
-    for k, v in b.items():
-        a[k] = v if a[k] < v else a[k]
-    return a
+def lcm(a, b):
+    g = gcd(a, b)
+    return a / g * b
 
-
-lcm_prime_factor = defaultdict(int)
+res = Ts[0]
 for T in Ts:
-    lcm_prime_factor = merge_prime_factor(lcm_prime_factor, get_prime_factor(T))
-
-res = 1
-for k, v in lcm_prime_factor.items():
-    res = res * (k ** v)
-print(res)
+    res = lcm(res, T)
+print(int(res))
