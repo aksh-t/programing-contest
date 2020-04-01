a80d360b3f398fdf6d5888c79d2050527b73b87a
aksh-t <tanno.ao@gmail.com>
2020-04-01 09:19:38 +0900

    rの最大値に対して、1回だけ篩を作る

--- atcoder/contests/abc084/tasks/abc084_d/main.py
+++ atcoder/contests/abc084/tasks/abc084_d/main.py
@@ -7,24 +7,10 @@ is_prime.append(False)
 is_prime.append(False)
 
 def sieve(r):
-    start = len(is_prime)
-    if start >= r:
-        return 
-
-    for i in range(start, r + 1):
+    for i in range(2, r + 1):
         is_prime.append(True)
 
-    for i in prime:
-        j = 2 * i
-        while j <= r:
-            if j < start:
-                j = j + i
-                continue
-
-            is_prime[j] = False
-            j = j + i
-
-    for i in range(start, r + 1):
+    for i in range(2, r + 1):
         if is_prime[i]:
             prime.append(i)
 
@@ -34,15 +20,19 @@ def sieve(r):
                 j = j + i
 
 def main():
-    answers = []
     Q = int(input())
+    querys = []
+    max_r = 0
     for i in range(Q):
         l, r = map(int, input().split())
-        sieve(r)
-        like_numbers = [n for n in prime if n % 2 == 1 and l <= n <= r and is_prime[(n + 1) // 2]]
-        answers.append(len(like_numbers))
-    
-    for answer in answers:
-        print(answer)
+        querys.append((l, r))
+        if max_r < r:
+            max_r = r
 
+    sieve(max_r)
+
+    for l, r in querys:
+        like_numbers = [n for n in prime if n % 2 == 1 and l <= n <= r and is_prime[(n + 1) // 2]]
+        print(len(like_numbers))
+    
 main()
