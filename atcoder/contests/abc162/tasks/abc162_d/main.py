ef4e7c0c9de793121ea2c2a86e4f4da6f3386808
aksh-t <tanno.ao@gmail.com>
2020-04-14 00:55:06 +0900

    解説と他の人の提出を見てAC

--- atcoder/contests/abc162/tasks/abc162_d/main.py
+++ atcoder/contests/abc162/tasks/abc162_d/main.py
@@ -2,22 +2,36 @@ def main():
     N = int(input())
     S = input()
 
-    answer = 0
+    R_count = 0
+    G_count = 0
+    B_count = 0
 
+    for s in S:
+        if s == "R":
+            R_count += 1
+        elif s == "G":
+            G_count += 1
+        else:
+            B_count += 1
+
+    # 第一条件だけを考えたときの組み合わせの総数
+    answer = R_count * G_count * B_count
+
+    # この二重ループが O(N^2)
     for i in range(N - 2):
-        Si = S[i]
         for j in range(i + 1, N - 1):
-            Sj = S[j]
-            if Si == Sj:
+            # i, j が決まると、第二条件を満たさない k が決まる
+            k = 2 * j - i
+
+            # k が文字列の長さを超えたときは、第二条件を満たすことができない
+            if k >= N:
                 continue
 
-            for k in range(j + 1, N):
-                Sk = S[k]
-                if Si == Sk or Sj == Sk:
-                    continue
-                if j - i == k - j:
-                    continue
-                answer += 1
+            # 第二条件を満たす i, j, k が、第一条件を満たす場合、
+            # 第一条件だけ考えたときの組み合わせの総数から差し引く
+            if S[i] != S[j] and S[i] != S[k] and S[j] != S[k]:
+                answer -= 1
+            
     print(answer)
 
 if __name__ == '__main__':
