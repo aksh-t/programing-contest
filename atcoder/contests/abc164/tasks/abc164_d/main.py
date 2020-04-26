ebd0fa7701e5d5db7eafc5b382387a5bd120c7b4
aksh-t <tanno.ao@gmail.com>
2020-04-26 22:34:28 +0900

    方向性は悪くない気がするが、181718171みたいなパターンに対処できない

--- atcoder/contests/abc164/tasks/abc164_d/main.py
+++ atcoder/contests/abc164/tasks/abc164_d/main.py
@@ -3,16 +3,14 @@ def main():
 
     # MAX_S = int("9" * 200000) # 大き過ぎる
     multipuls = []
-    answers = []
+    # answers = []
+    answer = 0
 
-    i = 1
-    while True:
-        if 2019 * i > MAX_S:
-            print(i)
-            break
-        multipuls.append(2019 * i)
-        i += 1
-        print(i, 2019 * i)
+    for i in range(1, 10000):
+        multipuls.append(str(2019 * i))
+
+    for multipul in multipuls:
+        answer += S.count(multipul)
 
     # for i in range(len(S) - 1):
     #     for j in range(i + 3, len(S)):
@@ -22,6 +20,7 @@ def main():
     #             answers.append((i + 1, j + 1))
 
     # print(len(answers))
+    print(answer)
 
 
 if __name__ == '__main__':
