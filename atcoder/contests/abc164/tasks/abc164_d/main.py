d54579fc9aaf54952617e535f4be6a8f8c245ecf
aksh-t <tanno.ao@gmail.com>
2020-05-02 01:06:00 +0900

    一旦処理を空にする

--- atcoder/contests/abc164/tasks/abc164_d/main.py
+++ atcoder/contests/abc164/tasks/abc164_d/main.py
@@ -1,25 +1,7 @@
 def main():
     S = input()
-
-    # MAX_S = int("9" * 200000) # 大き過ぎる
-    multipuls = []
-    # answers = []
     answer = 0
 
-    for i in range(1, 10000):
-        multipuls.append(str(2019 * i))
-
-    for multipul in multipuls:
-        answer += S.count(multipul)
-
-    # for i in range(len(S) - 1):
-    #     for j in range(i + 3, len(S)):
-    #         # print(S[i:j+1])
-    #         # print(i + 1, j + 1)
-    #         if int(S[i:j+1]) % 2019 == 0:
-    #             answers.append((i + 1, j + 1))
-
-    # print(len(answers))
     print(answer)
 
 
