c33adf75ae4be23a77f81abab0d56944a978796b
aksh-t <tanno.ao@gmail.com>
2020-05-02 01:53:27 +0900

    youtubeの解説動画のC++のアルゴリズムを、Pythonで実装してみたらTLE

--- atcoder/contests/abc164/tasks/abc164_d/main.py
+++ atcoder/contests/abc164/tasks/abc164_d/main.py
@@ -1,8 +1,41 @@
+from collections import defaultdict
+
 def main():
     S = input()
-    answer = 0
+    # S = "1" * 200000
+    S = S[::-1]
 
-    print(answer)
+    x = 1
+    tot = 0
+    cnt = defaultdict(int)
+    ans = 0
+
+    for i in range(len(S)):
+        # print("-----")
+        # print(f"i: {i}")
+        # print(f"tot: {tot}")
+        # print(f"S[i]: {S[i]}")
+        # print(f"x: {x}")
+        # print(f"cnt: {cnt}")
+        # print(f"cnt[tot]: {cnt[tot]}")
+        # print(f"ans: {ans}")
+
+        cnt[tot] += 1
+        tot += int(S[i]) * x
+        tot %= 2019
+        ans += cnt[tot]
+        x = x * 10
+    
+    # print("-----")
+    # print(f"i: {i}")
+    # print(f"tot: {tot}")
+    # print(f"S[i]: {S[i]}")
+    # print(f"x: {x}")
+    # print(f"cnt: {cnt}")
+    # print(f"cnt[tot]: {cnt[tot]}")
+    # print(f"ans: {ans}")
+    
+    print(ans)
 
 
 if __name__ == '__main__':
