4c5d458235802c4f0075bc416059fc5d6d85484f
aksh-t <tanno.ao@gmail.com>
2020-05-04 01:44:40 +0900

    ループを使わない別の解法

--- atcoder/contests/abc165/tasks/abc165_a/main.py
+++ atcoder/contests/abc165/tasks/abc165_a/main.py
@@ -4,12 +4,21 @@ def main():
     K = int(input())
     A, B = map(int, input().split())
 
-    for i in range(A, B + 1):
-        if i % K == 0:
-            print("OK")
-            exit()
+    # for i in range(A, B + 1):
+    #     if i % K == 0:
+    #         print("OK")
+    #         exit()
 
-    print("NG")
+    # print("NG")
+
+    if A % K == 0:
+        print("OK")
+        exit()
+    
+    if B // K > A // K:
+        print("OK")
+    else:
+        print("NG")
 
 if __name__ == '__main__':
     main()
