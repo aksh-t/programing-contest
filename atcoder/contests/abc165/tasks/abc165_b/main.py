f5ee24482ed879acc64b6239d915cb43101eaebb
aksh-t <tanno.ao@gmail.com>
2020-05-05 02:27:06 +0900

    AC

---
+++ atcoder/contests/abc165/tasks/abc165_b/main.py
@@ -0,0 +1,17 @@
+from sys import exit
+
+def main():
+    X = int(input())
+    deposit = 100
+
+    i = 0
+    while True:
+        deposit = int(deposit * 1.01)
+        i += 1
+
+        if deposit >= X:
+            print(i)
+            exit()
+
+if __name__ == '__main__':
+    main()
